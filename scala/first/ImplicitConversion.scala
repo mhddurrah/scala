7bf4532b429f6c9b51ace41bdb5067db6348edfe
MOHAMD DORRA <mhddurrah@users.noreply.github.com>
2016-06-13 03:03:49 +0300

    Update ImplicitConversion.scala

--- scala/first/ImplicitConversion.scala
+++ scala/first/ImplicitConversion.scala
@@ -2,15 +2,21 @@
   * Created by mhd.ug on 6/12/16.
   */
 object ImplicitConversion extends App {
+ /**
+  * implicit converter method from integer to complex number
+  */
   implicit def intToComplex(n: Int): Complex = new Complex(n, n)
 
   val c = new Complex()
   val imp = 2 * c
 
-  println(implicitly(imp))
+  println(imp)
 
 }
 
+/**
+ * complex number representation
+ */
 class Complex(val real: Int, val img: Int) {
 
   def this() = this(1, 1)
